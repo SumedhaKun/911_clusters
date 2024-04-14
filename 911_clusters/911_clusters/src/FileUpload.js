ca2b4c921cc42dec7675913a79fb7bf8e23c9ddf
Rachana <rachanaraju03@gmail.com>
2024-04-13 17:39:03 -0700

    updated upload feature

--- 911_clusters/911_clusters/src/FileUpload.js
+++ 911_clusters/911_clusters/src/FileUpload.js
@@ -1,34 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import { useDropzone } from "react-dropzone";
 import "./index.css";
+import axios from 'axios';
 
 function FileUpload({ open }) {
   const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({});
-  const files = acceptedFiles.map((file) =>(
+  const files_all = acceptedFiles.map((file) =>(
     <li key={file.path}>
         {file.path} - {file.size} bytes
     </li>
   ));
+  const [files, setFiles] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  function handleMultipleChange(event) {
+    setFiles([...event.target.files]);
+  }
+
+  function handleMultipleSubmit(event) {
+    event.preventDefault();
+    const url = 'http://localhost:3000/uploadFiles';
+    const formData = new FormData();
+    files.forEach((file, index) => {
+      formData.append(`file${index}`, file);
+    });
+
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+
+    axios.post(url, formData, config)
+      .then((response) => {
+        console.log(response.data);
+        setUploadedFiles(response.data.files);
+      })
+      .catch((error) => {
+        console.error("Error uploading files: ", error);
+      });
+  }  
   return (
     <div className="container">
         <div {...getRootProps({ className: "dropzone" })}>
-            <input className="input-zone" {...getInputProps()} />
+            <input className="input-zone" {...getInputProps()} onChange={handleMultipleChange} />
             <div className="text-center">
                 {isDragActive ? (<p className="dropzone-content">
                     Release to drop files
                 </p>
                 ) : (
                 <p className="dropzone-content">
-                    Drag and drop files here or click to select fiiles
+                    Drag and drop ALL files here or click to select files 
                 </p>
                 )}
-                <button type="button" onClick={open} className="btn">
+                <button type="button" onClick={open} className="btn" onChange={handleMultipleChange}>
                     Click to select files
                 </button>
-            </div>    
+            </div>
+            <button type='button' className="bttn" onSubmit={handleMultipleSubmit}>
+                Submit
+            </button>  
         </div>
+        {uploadedFiles.map((file, index) => (
+        <img key={index} src={file} alt={`Uploaded content ${index}`} />
+      ))}
         <aside>
-            <ul>{files}</ul>
+            <ul>{files_all}</ul>
         </aside>
     </div>
     
